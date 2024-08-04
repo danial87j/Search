9af0a5c6d17e05f50724788a2b76a5f075e19d09
Danial <126600890+danial87j@users.noreply.github.com>
2024-08-04 18:20:01 +0330

    fix script.js
    fixed some issues

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 // String is for the text and target is the word we are searching for.
-let string = "Hello World :D",
+let string = " Hello World :D ",
   target = "Hello";
-// The search method searches the text and finds the word you entered in the target.
+// Search searches through the text and find the word you put in target.
 target = string.search(`${target}`);
 //If The word exist it will print true if it doesnt it will print false.
-if (target >= 1){
-  console.log("True ,", string)
-}
-else{
-  console.log(`False Your Word Does Not Exist In ${string}`)
+if (target >= 1) {
+  console.log("True ,", string);
+} else {
+  console.log(`False Your Word Does Not Exist In ${string}`);
 }
